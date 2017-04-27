a33d37d6f76cc51891e0820bd56e791c512375ad
Michael Dekker <git@michaeldekker.nl>
2017-04-27 04:41:53 +0200

    Fix zip structure

--- build.sh
+++ build.sh
@@ -16,10 +16,11 @@ CWD_BASENAME=${PWD##*/}
 
 MODULE_VERSION="$(sed -ne "s/\\\$this->version *= *['\"]\([^'\"]*\)['\"] *;.*/\1/p" ${CWD_BASENAME}.php)"
 MODULE_VERSION=${MODULE_VERSION//[[:space:]]}
-ZIP_FILE="${CWD_BASENAME}-v${MODULE_VERSION}.zip"
+ZIP_FILE="${CWD_BASENAME}/${CWD_BASENAME}-v${MODULE_VERSION}.zip"
 
 echo "Going to zip ${CWD_BASENAME} version ${MODULE_VERSION}"
 
+cd ..
 for E in "${FILES[@]}"; do
-  find ${E}  -type f -exec zip -9 ${ZIP_FILE} {} \;
+  find ${CWD_BASENAME}/${E}  -type f -exec zip -9 ${ZIP_FILE} {} \;
 done
